f8d84121ad4fe61cfe3243afe62d7190f492964d
BaselAllam <baseljahen@gmail.com>
2021-07-06 21:52:59 +0200

    commit

---
+++ orga.dart
@@ -0,0 +1,17 @@
+// Functional Programming
+  // Organization
+  // Able to be customized
+  // Reusable
+  // Readble 
+
+
+// AVG 
+  // Sum
+  // Length
+
+
+  // 1 Fun => avg
+
+  // fun => sum
+  // fun => length
+  // fun => avg ( sum , length )
