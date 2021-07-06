3fa28522c8a6628eb73f835b06299de39e703f06
BaselAllam <baseljahen@gmail.com>
2021-07-06 22:04:07 +0200

    commit

--- orga.dart
+++ orga.dart
@@ -15,3 +15,15 @@
   // fun => sum
   // fun => length
   // fun => avg ( sum , length )
+
+
+
+  // Stds 
+      // name, email, age, upsent
+      // 5 Students
+
+
+      // 1- Validation for each email ( @, .com, lower case, white spaces )
+      // 2- validation if upsent > 5 : remove student
+      // 3- validation if upsent > 3 : add warning field into his map
+      // 4- avg students ages > reduce
