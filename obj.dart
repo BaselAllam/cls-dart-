612baea4457726d1559b83fc61158c96086e2633
BaselAllam <baseljahen@gmail.com>
2021-07-08 22:01:28 +0200

    commit

---
+++ obj.dart
@@ -0,0 +1,35 @@
+// OOP 
+    // 1- Readble
+    // 2- Customization
+    // 3- Perfomrance
+    
+
+    // 1- Capsulation
+    // 2- EnCapsulation
+    // 3- Inhertience
+
+
+    // 1- Class
+    // 2- Object
+    // 3- Constructor
+    // 4- Access Modifier
+    // 5- Methods & Properties
+    // 6- Inhertience 
+    // 7- Multiple Inhertience
+    // 8- Abstraction
+
+
+//--------------------------------------//
+
+
+    // User => Email, Name, Password, Confirm Password
+    // Calendar
+        // EachDay
+          // - Meals Type ( Breakfast, Dinner, Launch, Snacks )
+              // Each Meal Type 
+                  // - Number of Meals
+
+
+
+
+                  // output ( User Data, dates ( Meals Type ( Meals )))
