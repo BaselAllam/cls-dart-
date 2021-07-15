20350768dcca4522c74571a9bfb5428ee81b1a60
BaselAllam <baseljahen@gmail.com>
2021-07-15 17:56:51 +0200

    commit

---
+++ std/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+
+  runApp(HomeScreen());
+}
+
+
+
+class HomeScreen extends StatelessWidget{
+
+@override
+  Widget build(BuildContext context) {
+    return Text('ahmed');
+  }
+}
